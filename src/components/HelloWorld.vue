b92ece397b532971ce2815e84bc552baf4e02feb
Ahuzsp <2391910724@qq.com>
2020-11-19 23:21:25 +0800

    222

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,10 +20,15 @@
       :url-list="[url]"
       ref="aaa"
     />
-  <span class="demonstration">默认不区分颜色</span>
-  <el-rate v-model="value1"></el-rate>
-  <span class="demonstration">默认不区分颜色</span>
-  <el-rate v-model="value1"></el-rate>
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="活动名称">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+  <el-form-item label="活动区域">
+    <el-select v-model="form.region" placeholder="请选择活动区域">
+      <el-option label="区域一" value="shanghai"></el-option>
+      <el-option label="区域二" value="beijing"></el-option>
+    </el-select>
   </div>
 </template>
 

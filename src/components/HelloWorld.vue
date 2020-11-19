25435b372566848acf1af1ea0bb0a49d45cecc5b
Ahuzsp <2391910724@qq.com>
2020-11-19 22:37:01 +0800

    新建分支zsp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,93 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-upload
+      class="upload-demo"
+      action="http://127.0.0.1:8888/api/private/v1/upload"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove"
+      :on-success="handleSuccess"
+      :before-upload="beforeUp"
+      list-type="picture"
+    >
+      <el-button size="small" type="primary">点击上传</el-button>
+    </el-upload>
+    <!-- <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="" />
+    </el-dialog> -->
+    <el-image-viewer
+      v-show="showViewer"
+      :on-close="closeViewer"
+      :url-list="[url]"
+      ref="aaa"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    "el-image-viewer": () =>
+      import("element-ui/packages/image/src/image-viewer"),
+  },
+  data() {
+    return {
+      dialogImageUrl: "",
+      dialogVisible: false,
+      // 缩放框
+      showViewer: false,
+      url:
+        "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1cb0358a55614d5f49fa442b1b056cc2.jpg?w=2452&h=920",
+    };
+  },
+  updated() {
+    // const imgNodeValue = this.$refs.aaa.$refs.img[0].attributes[2].nodeValue
+    // imgNodeValue = "transform: scale(1) rotate(0deg); margin-left: 0px; margin-top: 0px; max-height: 100%; max-width: 80%"
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      console.log(file);
+      this.showViewer = true;
+      this.$nextTick(()=>{
+        console.log(this.$refs.aaa);
+        this.$refs.aaa.$refs.img[0].attributes[2].nodeValue = "transform: scale(1) rotate(0deg); margin-left: 0px; margin-top: 0px; max-height: 100%; max-width: 80%"
+      })
+    },
+    beforeUp(file) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var txt = event.target.result;
+        var img = document.createElement("img");
+        img.src = txt;
+        img.onload = function () {
+          console.log(img.width);
+          console.log(img.height);
+        };
+      };
+      reader.readAsDataURL(file);
+    },
+    handleSuccess(response) {
+      console.log(response);
+    },
+    closeViewer() {
+      this.showViewer = false;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* .el-image-viewer__btn .el-image-viewer__close {
+    color: #FFF;
+    opacity: unset;
+    top: 50px;
+    right: 50px;
+    width: 50px;
+    height: 50px;
+    font-size: 50px;
+} */
+
 </style>
